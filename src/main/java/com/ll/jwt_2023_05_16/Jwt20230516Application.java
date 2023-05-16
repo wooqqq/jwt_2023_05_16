e5949195a5b059b89ec1c501793136811d3ef077
깃wooqqq <sge0225@naver.com>
2023-05-16 14:40:31 +0900

    세팅

---
+++ src/main/java/com/ll/jwt_2023_05_16/Jwt20230516Application.java
@@ -0,0 +1,13 @@
+package com.ll.jwt_2023_05_16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwt20230516Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jwt20230516Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/jwt_2023_05_16/Jwt20230516ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.jwt_2023_05_16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jwt20230516ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
