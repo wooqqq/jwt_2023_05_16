ba61f4e7b4979091188d277c5e4dd59f3b84bc79
깃wooqqq <sge0225@naver.com>
2023-05-16 15:45:33 +0900

    feat : 토큰 인증

--- src/main/java/com/ll/jwt_2023_05_16/util/Ut.java
+++ src/main/java/com/ll/jwt_2023_05_16/util/Ut.java
@@ -3,6 +3,7 @@ package com.ll.jwt_2023_05_16.util;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Ut {
@@ -15,5 +16,14 @@ public class Ut {
                 return null;
             }
         }
+
+        public static Map<String, Object> toMap(String jsonStr) {
+            try {
+                return new ObjectMapper().readValue(jsonStr, LinkedHashMap.class);
+            } catch (JsonProcessingException e) {
+                return null;
+            }
+        }
+
     }
 }
